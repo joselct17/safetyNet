eb3095c3e66f72d3e3fc691ee5a4cedb0e237f7f
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-01-24 11:15:45 +0100

    rajout du Post firestation

--- src/main/java/com/safetynet/safetyNet/controller/FireStationController.java
+++ src/main/java/com/safetynet/safetyNet/controller/FireStationController.java
@@ -3,11 +3,17 @@ package com.safetynet.safetyNet.controller;
 
 import com.safetynet.safetyNet.dao.FireStationDao;
 import com.safetynet.safetyNet.model.FireStation;
+import com.safetynet.safetyNet.model.Person;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
-
+import java.util.Objects;
 
 
 @RestController
@@ -24,5 +30,21 @@ public class FireStationController {
     public List<FireStation> showFirestation() {
 
         return fireStationDao.findAll();
+    }
+
+    @PostMapping("/firestation")
+    public ResponseEntity<FireStation> ajouterFirestation(@RequestBody FireStation fireStation) {
+        FireStation firestationAdded = fireStationDao.save(fireStation);
+        if (Objects.isNull(firestationAdded)) {
+            return ResponseEntity.noContent().build();
+        }
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{stationNumber}")
+                .buildAndExpand(firestationAdded.getStationNumber())
+                .toUri();
+        return ResponseEntity.created(location).build();
+
     }
 }
