6543bea0a2264bb6898b36f0a53214fe98e071fc
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-01-24 11:16:54 +0100

    rajout du POST medicalRecord

--- src/main/java/com/safetynet/safetyNet/controller/MedicalRecordsController.java
+++ src/main/java/com/safetynet/safetyNet/controller/MedicalRecordsController.java
@@ -1,11 +1,18 @@
 package com.safetynet.safetyNet.controller;
 
 import com.safetynet.safetyNet.dao.MedicalRecordsDao;
+import com.safetynet.safetyNet.model.FireStation;
 import com.safetynet.safetyNet.model.MedicalRecords;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class MedicalRecordsController {
@@ -20,5 +27,21 @@ public class MedicalRecordsController {
     @GetMapping("/medicalRecord")
     List<MedicalRecords> medicalList() {
         return medicalRecordsDao.findAll();
+    }
+
+    @PostMapping("/medicalRecord")
+    public ResponseEntity<MedicalRecords> ajouterMedical(@RequestBody MedicalRecords medicalRecords) {
+        MedicalRecords medicalAdded = medicalRecordsDao.save(medicalRecords);
+        if (Objects.isNull(medicalAdded)) {
+            return ResponseEntity.noContent().build();
+        }
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{lastName}")
+                .buildAndExpand(medicalAdded.getLastName())
+                .toUri();
+        return ResponseEntity.created(location).build();
+
     }
 }
