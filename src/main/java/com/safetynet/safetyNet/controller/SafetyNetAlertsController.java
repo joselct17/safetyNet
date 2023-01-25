0ed8c4ec8d743ab0d8c23b7914cc80a7c584cd34
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-01-25 08:43:46 +0100

    creation des requettes peopleByCity, childs and peopleByFirestation

--- src/main/java/com/safetynet/safetyNet/controller/SafetyNetAlertsController.java
+++ src/main/java/com/safetynet/safetyNet/controller/SafetyNetAlertsController.java
@@ -3,8 +3,8 @@ package com.safetynet.safetyNet.controller;
 import com.safetynet.safetyNet.dao.PersonDao;
 import com.safetynet.safetyNet.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,15 +14,28 @@ import java.util.List;
 public class SafetyNetAlertsController {
 
     @Autowired
-
     private PersonDao personDao;
 
 
-    @GetMapping("/communityEmail")
-    public List<Person> listEmails() {
 
-        return personDao.getEmails();
+    //http://localhost:8080/communityEmail?city=<city>
+    @GetMapping("/communityEmail/{city}")
+    public List<Person> peopleByCity(@PathVariable String city) {
 
+        return personDao.getEmailsByCity(city);
+
+    }
+
+    //http://localhost:8080/firestation?stationNumber=%3Cstation_number
+    @GetMapping("/firestation/{stationNumber}")
+    public List<Person> peopleByFirestation(@PathVariable String stationNumber) {
+        return null;
+    }
+
+    //http://localhost:8080/childAlert?address=%3Caddress
+    @GetMapping("/childAlert/{address}")
+    public List<Person> childs(@PathVariable String address) {
+        return personDao.getChildsByAddress(address);
     }
 
 }
