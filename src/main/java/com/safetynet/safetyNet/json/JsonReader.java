b8df1b1f69f4cab22212bdd5ddc2a5605dc664b2
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-01-25 08:39:00 +0100

    le service jsonReader alimente les les DaoImpl

--- src/main/java/com/safetynet/safetyNet/json/JsonReader.java
+++ src/main/java/com/safetynet/safetyNet/json/JsonReader.java
@@ -7,15 +7,29 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Component;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
-
+@Component
 public class JsonReader {
 
-    public JsonReader() throws ParseException, IOException, FileNotFoundException {
+    public final ArrayList<Person> listPersons;
+
+    public final ArrayList<FireStation> listFireStation;
+
+    public final ArrayList<MedicalRecords> listMedicalRecords;
+
+
+    public JsonReader(ArrayList<Person> listPersons, ArrayList<FireStation> listFireStation, ArrayList<MedicalRecords> listMedicalRecords ) throws ParseException, IOException, FileNotFoundException {
+
+
+        this.listPersons = listPersons;
+        this.listFireStation = listFireStation;
+        this.listMedicalRecords = listMedicalRecords;
 
         JSONParser parser = new JSONParser();
         FileReader reader = new FileReader("src/main/resources/JSON/data.JSON");
@@ -24,24 +38,59 @@ public class JsonReader {
 
         JSONObject jsonObject = (JSONObject) jsonObj;
 
-        JSONArray persons = (JSONArray) jsonObject.get("persons");
-        Iterator<Person> per = persons.iterator();
+        JSONArray persons = (JSONArray ) jsonObject.get("persons");
+
+        Iterator<JSONObject> per = persons.iterator();
+
         while (per.hasNext()) {
-            System.out.println("Person = " + per.next());
+
+            JSONObject o = per.next();
+            Person person = new Person();
+            person.setFirstName((String)o.get("firstName"));
+            person.setLastName((String)o.get("lastName"));
+            person.setAddress((String)o.get("address"));
+            person.setCity((String)o.get("city"));
+            person.setZip((String)o.get("zip"));
+            person.setTelephone((String)o.get("phone"));
+            person.setEmail((String)o.get("email"));
+
+            listPersons.add(person);
+
         }
 
-        JSONArray firestations = (JSONArray) jsonObject.get("firestations");
-        Iterator<FireStation> fire = firestations.iterator();
+        JSONArray firestations = (JSONArray ) jsonObject.get("firestations");
+
+        Iterator<JSONObject> fire = firestations.iterator();
+
         while (fire.hasNext()) {
-            System.out.println("Firestation = " + fire.next());
+
+            JSONObject o = fire.next();
+            FireStation fireStation = new FireStation();
+            fireStation.setAddress((String)o.get("address"));
+            fireStation.setStationNumber((String)o.get("station"));
+
+            listFireStation.add(fireStation);
+
         }
 
-        JSONArray medicalrecords = (JSONArray) jsonObject.get("medicalrecords");
-        Iterator<MedicalRecords> medic = medicalrecords.iterator();
+        JSONArray medicalrecords = (JSONArray ) jsonObject.get("medicalrecords");
+
+        Iterator<JSONObject> medic = medicalrecords.iterator();
+
         while (medic.hasNext()) {
-            System.out.println("Medicalrecords = " + medic.next());
+
+            JSONObject o = medic.next();
+            MedicalRecords medicalRec = new MedicalRecords();
+            medicalRec.setFirstName((String)o.get("firstName"));
+            medicalRec.setLastName((String)o.get("lastName"));
+            medicalRec.setBirthDate((String)o.get("birthdate"));
+            medicalRec.setMedication((ArrayList<String>) o.get("medications"));
+            medicalRec.setAllergies((ArrayList<String>) o.get("allergies"));
+
+            listMedicalRecords.add(medicalRec);
+
+
         }
-        reader.close();
     }
 
 }
