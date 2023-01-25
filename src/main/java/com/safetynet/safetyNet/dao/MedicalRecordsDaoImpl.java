99cc7b68e651c1d6013d9d0f8ec8cb34ee8b08bc
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-01-25 08:40:37 +0100

    utilisation du fichier jsonReader et requette save

--- src/main/java/com/safetynet/safetyNet/dao/MedicalRecordsDaoImpl.java
+++ src/main/java/com/safetynet/safetyNet/dao/MedicalRecordsDaoImpl.java
@@ -1,66 +1,50 @@
 package com.safetynet.safetyNet.dao;
 
-import com.safetynet.safetyNet.model.FireStation;
+import com.safetynet.safetyNet.json.JsonReader;
 import com.safetynet.safetyNet.model.MedicalRecords;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @Repository
-public class MedicalRecordsDaoImpl implements MedicalRecordsDao{
-
-    private final ArrayList<MedicalRecords> listMedicalRecords;
-
-    public MedicalRecordsDaoImpl(ArrayList<MedicalRecords> medicalRecords) throws IOException, ParseException {
-        this.listMedicalRecords = medicalRecords;
-
-        JSONParser parser = new JSONParser();
-        FileReader reader = new FileReader("src/main/resources/JSON/data.JSON");
-
-        Object jsonObj = parser.parse(reader);
-
-        JSONObject jsonObject = (JSONObject) jsonObj;
-
-        JSONArray jsonArray = (JSONArray ) jsonObject.get("medicalrecords");
-
-        Iterator<JSONObject> i = jsonArray.iterator();
-
-        while (i.hasNext()) {
-
-            JSONObject o = i.next();
-            MedicalRecords medicalRec = new MedicalRecords();
-            medicalRec.setFirstName((String)o.get("firstName"));
-            medicalRec.setLastName((String)o.get("lastName"));
-            medicalRec.setBirthDate((String)o.get("birthdate"));
-            medicalRec.setMedication((ArrayList<String>) o.get("medications"));
-            medicalRec.setAllergies((ArrayList<String>) o.get("allergies"));
-
-            listMedicalRecords.add(medicalRec);
+public class MedicalRecordsDaoImpl implements MedicalRecordsDao {
 
 
-        }
+    @Autowired
+    private final JsonReader jsonReader;
+
+    public MedicalRecordsDaoImpl(JsonReader jsonReader) {
+
+        this.jsonReader = jsonReader;
+
     }
-
 
     @Override
     public List<MedicalRecords> findAll() {
-        return listMedicalRecords;
+        return jsonReader.listMedicalRecords;
     }
 
     @Override
     public MedicalRecords save(MedicalRecords medicalRecords) {
-        listMedicalRecords.add(medicalRecords);
+        jsonReader.listMedicalRecords.add(medicalRecords);
         return medicalRecords;
     }
 
+    @Override
+    public MedicalRecords update(MedicalRecords medicalRecords) {
+        for (MedicalRecords m : jsonReader.listMedicalRecords) {
+            if (m.getFirstName().equals(medicalRecords.getFirstName()) && m.getLastName().equals(medicalRecords.getLastName())) {
+                m.setBirthDate(medicalRecords.getBirthDate());
+                m.setMedication(new ArrayList<>(medicalRecords.getMedication()));
+                m.setAllergies(new ArrayList<>(medicalRecords.getAllergies()));
+            }
+        }
+
+        return medicalRecords;
+
+    }
 
 }
