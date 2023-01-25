20941acf2b58f555f96e1dc9ea5b7e78993654af
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-01-25 08:38:06 +0100

    rajout du service jsonReader

--- src/main/java/com/safetynet/safetyNet/dao/FireStationDaoImpl.java
+++ src/main/java/com/safetynet/safetyNet/dao/FireStationDaoImpl.java
@@ -1,61 +1,37 @@
 package com.safetynet.safetyNet.dao;
 
+import com.safetynet.safetyNet.json.JsonReader;
 import com.safetynet.safetyNet.model.FireStation;
-import com.safetynet.safetyNet.model.Person;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Repository
 public class FireStationDaoImpl implements FireStationDao {
 
-    private ArrayList<FireStation> listFireStation;
+    @Autowired
+    private JsonReader jsonReader;
 
-    public FireStationDaoImpl(ArrayList<FireStation> listFireStation) throws IOException, ParseException {
-        this.listFireStation = listFireStation;
-
-        JSONParser parser = new JSONParser();
-        FileReader reader = new FileReader("src/main/resources/JSON/data.JSON");
-
-        Object jsonObj = parser.parse(reader);
-
-        JSONObject jsonObject = (JSONObject) jsonObj;
-
-        JSONArray jsonArray = (JSONArray ) jsonObject.get("firestations");
-
-        Iterator<JSONObject> i = jsonArray.iterator();
-
-        while (i.hasNext()) {
-
-            JSONObject o = i.next();
-            FireStation fireStation = new FireStation();
-            fireStation.setAddress((String)o.get("address"));
-            fireStation.setStationNumber((String)o.get("station"));
-
-            listFireStation.add(fireStation);
-
-        }
+    public FireStationDaoImpl( JsonReader jsonReader)  {
+        this.jsonReader = jsonReader;
     }
 
 
     @Override
     public List<FireStation> findAll() {
-        return listFireStation;
+        return jsonReader.listFireStation;
     }
 
     @Override
-    public List<FireStation> findByAddress() {
-        return null;
+    public List<FireStation> getByAddress(String address) {
+
+        return jsonReader.listFireStation.stream().filter(fireStation -> fireStation.getAddress().equals(address)).collect(Collectors.toList());
     }
 
     @Override
@@ -63,9 +39,20 @@ public class FireStationDaoImpl implements FireStationDao {
         return null;
     }
 
+
     @Override
     public FireStation save(FireStation fireStation) {
-        listFireStation.add(fireStation);
+        jsonReader.listFireStation.add(fireStation);
+        return fireStation;
+    }
+
+    @Override
+    public FireStation update(FireStation fireStation) {
+        for (FireStation f : jsonReader.listFireStation) {
+            if (f.getAddress().equals(fireStation.getAddress())) {
+                f.setStationNumber(fireStation.getStationNumber());
+            }
+        }
         return fireStation;
     }
 
