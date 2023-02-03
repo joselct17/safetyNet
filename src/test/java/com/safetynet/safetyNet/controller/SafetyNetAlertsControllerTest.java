d5cb5779b576b1807f3ae21d2e2d8f850df72872
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-03 13:42:09 +0100

    rajout des test

--- src/test/java/com/safetynet/safetyNet/controller/SafetyNetAlertsControllerTest.java
+++ src/test/java/com/safetynet/safetyNet/controller/SafetyNetAlertsControllerTest.java
@@ -1,36 +1,29 @@
 package com.safetynet.safetyNet.controller;
 
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-import com.jsoniter.JsonIterator;
-import com.jsoniter.ValueType;
-import com.jsoniter.any.Any;
-import com.safetynet.safetyNet.json.JsonReader;
-import com.safetynet.safetyNet.model.FireStation;
-import com.safetynet.safetyNet.model.MedicalRecords;
+import com.safetynet.safetyNet.dao.IPersonDao;
 import com.safetynet.safetyNet.model.Person;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import com.safetynet.safetyNet.service.ISafetyNetAlertsService;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Collection;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 
 
 @SpringBootTest
@@ -41,9 +34,34 @@ class SafetyNetAlertsControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Mock
+    ISafetyNetAlertsService iSafetyNetMock;
+
+    @InjectMocks
+    IPersonDao iPersonDao;
+
+    List<Person> personEmailListforCulver;
+
+
+    @BeforeEach
+
+    void initializeData() {
+
+        personEmailListforCulver = new ArrayList<>((Collection) new Person("Jon", "Doe", "101 Av", "Culver", "78780", "555", "d@d.com"));
+    }
 
     @Test
     void testGetEmailsInCity() throws Exception {
+
+        JSONObject expected = new JSONObject();
+
+    expected.put("email", "a@a.com" );
+    expected.put("email", "b@b.com");
+    expected.put("email", "c@c.com");
+
+    when(iPersonDao.getByCity("Culver")).thenReturn(personEmailListforCulver);
+    //when(iPersonDao.findAll()).thenReturn();
+
         mockMvc.perform(get("/communityEmail/Culver"))
                 .andDo(print())
                 .andExpect(status().isOk());
