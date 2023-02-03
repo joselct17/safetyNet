7e78ff7ad222a9a0aa2ac843c7f1de1f39e835af
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-03 13:31:04 +0100

    rajout requette delete

--- src/main/java/com/safetynet/safetyNet/dao/FireStationDaoImpl.java
+++ src/main/java/com/safetynet/safetyNet/dao/FireStationDaoImpl.java
@@ -2,18 +2,16 @@ package com.safetynet.safetyNet.dao;
 
 import com.safetynet.safetyNet.json.JsonReader;
 import com.safetynet.safetyNet.model.FireStation;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
 @Repository
-public class FireStationDaoImpl implements FireStationDao {
+public class FireStationDaoImpl implements IFireStationDao {
 
     @Autowired
     private JsonReader jsonReader;
@@ -29,14 +27,18 @@ public class FireStationDaoImpl implements FireStationDao {
     }
 
     @Override
-    public List<FireStation> getByAddress(String address) {
+    public String getByAddress(String address) {
 
-        return jsonReader.listFireStation.stream().filter(fireStation -> fireStation.getAddress().equals(address)).collect(Collectors.toList());
+        List<FireStation> result = jsonReader.listFireStation.stream().filter(fireStation -> fireStation.getAddress().equals(address)).collect(Collectors.toList());
+        if (result.size()==1) {
+            return result.get(0).getStationNumber();
+        }
+        else return null;
     }
 
     @Override
-    public List<FireStation> findByNumber() {
-        return null;
+    public List<FireStation> getByStationNumber(String stationNumber) {
+        return jsonReader.listFireStation.stream().filter(fireStation -> fireStation.getStationNumber().equals(stationNumber)).collect(Collectors.toList());
     }
 
 
@@ -54,6 +56,15 @@ public class FireStationDaoImpl implements FireStationDao {
             }
         }
         return fireStation;
+    }
+
+    @Override
+    public void  delete(String address, String stationNumber) {
+
+        jsonReader.listFireStation.removeIf(fireStation ->
+                (fireStation.getStationNumber().equals(stationNumber)
+                        && fireStation.getAddress().equals(address)));
+
     }
 
 }
