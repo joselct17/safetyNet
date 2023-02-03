403d011bc6deef6063839bd57d69297c8a9c1031
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-03 13:39:09 +0100

    rajout requette GetByName

--- src/main/java/com/safetynet/safetyNet/dao/MedicalRecordsDaoImpl.java
+++ src/main/java/com/safetynet/safetyNet/dao/MedicalRecordsDaoImpl.java
@@ -8,9 +8,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
-public class MedicalRecordsDaoImpl implements MedicalRecordsDao {
+public class MedicalRecordsDaoImpl implements IMedicalRecordsDao {
 
 
     @Autowired
@@ -28,6 +29,19 @@ public class MedicalRecordsDaoImpl implements MedicalRecordsDao {
     }
 
     @Override
+    public MedicalRecords getByName(String firstName, String lastName) {
+        List<MedicalRecords> result = jsonReader.listMedicalRecords.stream()
+                .filter(medicalRecords -> (medicalRecords.getFirstName().equals(firstName) &&
+                        medicalRecords.getLastName().equals(lastName)))
+                .collect(Collectors.toList());
+        if (result.size()==1) {
+            return result.get(0);
+        }
+        else return null;
+    }
+
+
+    @Override
     public MedicalRecords save(MedicalRecords medicalRecords) {
         jsonReader.listMedicalRecords.add(medicalRecords);
         return medicalRecords;
@@ -37,7 +51,7 @@ public class MedicalRecordsDaoImpl implements MedicalRecordsDao {
     public MedicalRecords update(MedicalRecords medicalRecords) {
         for (MedicalRecords m : jsonReader.listMedicalRecords) {
             if (m.getFirstName().equals(medicalRecords.getFirstName()) && m.getLastName().equals(medicalRecords.getLastName())) {
-                m.setBirthDate(medicalRecords.getBirthDate());
+                m.setBirthDate (medicalRecords.getBirthDate());
                 m.setMedication(new ArrayList<>(medicalRecords.getMedication()));
                 m.setAllergies(new ArrayList<>(medicalRecords.getAllergies()));
             }
@@ -45,6 +59,11 @@ public class MedicalRecordsDaoImpl implements MedicalRecordsDao {
 
         return medicalRecords;
 
+    }
+
+    @Override
+    public void delete(String firstName, String lastName) {
+        jsonReader.listMedicalRecords.removeIf(medicalRecords -> (medicalRecords.getFirstName().equals(firstName) && medicalRecords.getLastName().equals(lastName)));
     }
 
 }
