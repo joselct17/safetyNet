233de9a5648b24bc4e71ddce1dded80305ac4451
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-03 13:29:56 +0100

    rajout mapping Delete

--- src/main/java/com/safetynet/safetyNet/controller/FireStationController.java
+++ src/main/java/com/safetynet/safetyNet/controller/FireStationController.java
@@ -1,9 +1,12 @@
 package com.safetynet.safetyNet.controller;
 
 
-import com.safetynet.safetyNet.dao.FireStationDao;
+import com.safetynet.safetyNet.dao.IFireStationDao;
 import com.safetynet.safetyNet.model.FireStation;
-import com.safetynet.safetyNet.model.Person;
+import com.safetynet.safetyNet.service.FireStationServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -17,21 +20,28 @@ import java.util.Objects;
 public class FireStationController {
 
 
-    private final FireStationDao fireStationDao;
+    private final FireStationServiceImpl fireStationService;
 
-    public FireStationController(FireStationDao fireStationDao) {
+    private final IFireStationDao fireStationDao;
+
+    private static final Logger logger = LoggerFactory.getLogger(FireStationController.class);
+
+    public FireStationController(FireStationServiceImpl fireStationService, IFireStationDao fireStationDao) {
+        this.fireStationService = fireStationService;
         this.fireStationDao = fireStationDao;
     }
 
     @GetMapping("/firestation")
     public List<FireStation> showFirestation() {
-
+        logger.info("GET /firestation called");
         return fireStationDao.findAll();
     }
 
     @PostMapping("/firestation")
     public ResponseEntity<FireStation> ajouterFirestation(@RequestBody FireStation fireStation) {
-        FireStation firestationAdded = fireStationDao.save(fireStation);
+
+        logger.info("POST /firestation called");
+        FireStation firestationAdded = fireStationService.saveFirestation(fireStation);
         if (Objects.isNull(firestationAdded)) {
             return ResponseEntity.noContent().build();
         }
@@ -41,6 +51,8 @@ public class FireStationController {
                 .path("/{stationNumber}")
                 .buildAndExpand(firestationAdded.getStationNumber())
                 .toUri();
+
+        logger.info("POST /firestation response : 201");
         return ResponseEntity.created(location).build();
 
     }
@@ -48,7 +60,7 @@ public class FireStationController {
     @PutMapping("/firestation")
     public ResponseEntity<FireStation> updateFirestation(@RequestBody FireStation fireStation) {
 
-        FireStation fireStationUpdate = fireStationDao.update(fireStation);
+        FireStation fireStationUpdate = fireStationService.updateFirestation(fireStation);
 
         if(Objects.isNull(fireStationUpdate)) {
             return ResponseEntity.noContent().build();
@@ -62,4 +74,13 @@ public class FireStationController {
         return ResponseEntity.created(location).build();
 
     }
+
+
+    @DeleteMapping("/firestation")
+    public ResponseEntity<FireStation> daleteFirestation(@RequestParam(required = true) String address, @RequestParam(required = true) String stationNumber) {
+        fireStationService.deleteFirestation(address, stationNumber);
+
+        return new ResponseEntity<>(HttpStatus.GONE);
+    }
+
 }
