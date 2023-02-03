7cdfb8ea75d106085cfdcd1833a91c672866a4a4
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-03 13:33:00 +0100

    rajout requette delete

--- src/main/java/com/safetynet/safetyNet/dao/IMedicalRecordsDao.java
+++ src/main/java/com/safetynet/safetyNet/dao/IMedicalRecordsDao.java
@@ -4,15 +4,19 @@ import com.safetynet.safetyNet.model.MedicalRecords;
 
 import java.util.List;
 
-public interface MedicalRecordsDao {
+public interface IMedicalRecordsDao {
 
     List<MedicalRecords> findAll();
+
+    MedicalRecords getByName(String firstName, String lastName);
 
 
     MedicalRecords save(MedicalRecords medicalRecords);
 
 
     MedicalRecords update(MedicalRecords medicalRecords);
+
+    void delete(String firstName, String lastName);
 
 
 
