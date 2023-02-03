c3ced84aefd5b6577a2b7c09eea0ca0884126005
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-03 13:41:32 +0100

    rajout des Rest mappings peopleByCity, childs, phoneNumber, peopleByFireStation

--- src/main/java/com/safetynet/safetyNet/controller/SafetyNetAlertsController.java
+++ src/main/java/com/safetynet/safetyNet/controller/SafetyNetAlertsController.java
@@ -1,12 +1,15 @@
 package com.safetynet.safetyNet.controller;
 
-import com.safetynet.safetyNet.dao.PersonDao;
 import com.safetynet.safetyNet.model.Person;
+import com.safetynet.safetyNet.service.ISafetyNetAlertsService;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
 import java.util.List;
 
 
@@ -14,28 +17,61 @@ import java.util.List;
 public class SafetyNetAlertsController {
 
     @Autowired
-    private PersonDao personDao;
+    private ISafetyNetAlertsService safetyNetAlertsService;
 
 
 
     //http://localhost:8080/communityEmail?city=<city>
     @GetMapping("/communityEmail/{city}")
-    public List<Person> peopleByCity(@PathVariable String city) {
+    public JSONObject peopleByCity(@PathVariable String city) {
 
-        return personDao.getEmailsByCity(city);
+        JSONObject peopleByCity = new JSONObject();
+        JSONArray result = new JSONArray();
+
+        result.add(safetyNetAlertsService.getEmailsByCity(city));
+
+        peopleByCity.put("Emails", result);
+
+        return peopleByCity;
 
     }
 
-    //http://localhost:8080/firestation?stationNumber=%3Cstation_number
-    @GetMapping("/firestation/{stationNumber}")
-    public List<Person> peopleByFirestation(@PathVariable String stationNumber) {
-        return null;
-    }
 
     //http://localhost:8080/childAlert?address=%3Caddress
     @GetMapping("/childAlert/{address}")
-    public List<Person> childs(@PathVariable String address) {
-        return personDao.getChildsByAddress(address);
+    public JSONObject childs(@PathVariable String address) throws ParseException {
+
+        JSONObject childs = new JSONObject();
+        JSONArray result = new JSONArray();
+
+        result.add(safetyNetAlertsService.getChildsByAddress(address));
+
+        childs.put("Childs", result);
+
+        return childs ;
     }
+
+
+    //http://localhost:8080/phoneAlert?firestation=%3Cfirestation_number
+    @GetMapping("/phoneAlert/{stationNumber}")
+    public JSONObject phoneNumber(@PathVariable String stationNumber)  {
+
+        JSONObject phoneNumber = new JSONObject();
+        JSONArray result = new JSONArray();
+
+        result.add(safetyNetAlertsService.getPhoneNumberForStationNumber(stationNumber));
+        phoneNumber.put("Phone" ,result);
+
+        return phoneNumber;
+    }
+
+
+    @GetMapping("/firestation/{stationNumber}")
+    public JSONObject peopleByFireStation(@PathVariable String stationNumber) {
+
+      return safetyNetAlertsService.getPeopleByStation(stationNumber);
+
+    }
+
 
 }
