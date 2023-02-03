f1b2682e3b06e86241890c4c3d1a35c64b024c17
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-03 13:36:36 +0100

    implementation des requettes service SafetyNet

--- src/main/java/com/safetynet/safetyNet/service/ISafetyNetAlertsService.java
+++ src/main/java/com/safetynet/safetyNet/service/ISafetyNetAlertsService.java
@@ -1,7 +1,25 @@
 package com.safetynet.safetyNet.service;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Set;
+
+@Service
 public interface ISafetyNetAlertsService {
 
 
+    Set<String> getEmailsByCity(String city);
 
+
+    ArrayList<JSONArray> getChildsByAddress(String address) throws ParseException;
+
+
+    JSONObject getPeopleByStation(String stationNumber);
+
+
+    Set<String> getPhoneNumberForStationNumber(String stationNumber);
 }
