86cfc878128f3123ba69f02fb485ad8286c3e581
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-07 17:17:20 +0100

    rajout de setFireStationList

--- src/main/java/com/safetynet/safetyNet/dao/FireStationDaoImpl.java
+++ src/main/java/com/safetynet/safetyNet/dao/FireStationDaoImpl.java
@@ -2,10 +2,12 @@ package com.safetynet.safetyNet.dao;
 
 import com.safetynet.safetyNet.json.JsonReader;
 import com.safetynet.safetyNet.model.FireStation;
+import com.safetynet.safetyNet.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,6 +20,10 @@ public class FireStationDaoImpl implements IFireStationDao {
 
     public FireStationDaoImpl( JsonReader jsonReader)  {
         this.jsonReader = jsonReader;
+    }
+
+    protected void setFireStationList(ArrayList<FireStation> listFireStation) {
+        this.jsonReader.listFireStation = listFireStation;
     }
 
 
