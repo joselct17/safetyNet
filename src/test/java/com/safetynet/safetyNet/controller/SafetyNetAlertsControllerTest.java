623d04fdc2320b67f41a6e3b3a0027838c5fab96
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-08 10:29:45 +0100

    implementation des test des requettes REST

--- src/test/java/com/safetynet/safetyNet/controller/SafetyNetAlertsControllerTest.java
+++ src/test/java/com/safetynet/safetyNet/controller/SafetyNetAlertsControllerTest.java
@@ -7,11 +7,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-import com.safetynet.safetyNet.dao.IPersonDao;
-import com.safetynet.safetyNet.model.Person;
+
+import com.safetynet.safetyNet.dao.PersonDaoImpl;
 import com.safetynet.safetyNet.service.ISafetyNetAlertsService;
-import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -21,9 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+
 
 
 @SpringBootTest
@@ -38,34 +34,10 @@ class SafetyNetAlertsControllerTest {
     ISafetyNetAlertsService iSafetyNetMock;
 
     @InjectMocks
-    IPersonDao iPersonDao;
-
-    List<Person> personEmailListforCulver;
+    PersonDaoImpl personDaoCUT;
 
 
-    @BeforeEach
 
-    void initializeData() {
-
-        personEmailListforCulver = new ArrayList<>((Collection) new Person("Jon", "Doe", "101 Av", "Culver", "78780", "555", "d@d.com"));
-    }
-
-    @Test
-    void testGetEmailsInCity() throws Exception {
-
-        JSONObject expected = new JSONObject();
-
-    expected.put("email", "a@a.com" );
-    expected.put("email", "b@b.com");
-    expected.put("email", "c@c.com");
-
-    when(iPersonDao.getByCity("Culver")).thenReturn(personEmailListforCulver);
-    //when(iPersonDao.findAll()).thenReturn();
-
-        mockMvc.perform(get("/communityEmail/Culver"))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
 
     @Test
     void  testPeopleByFirestation() throws Exception {
@@ -75,14 +47,48 @@ class SafetyNetAlertsControllerTest {
     }
 
     @Test
-    void  childsByAddress() throws Exception {
+    void  childsAlert() throws Exception {
         mockMvc.perform(get("/childAlert/101 Gotham City"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
 
+    @Test
+    void phoneNumber() throws Exception {
+        mockMvc.perform(get("/phoneAlert/2"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 
+    @Test
+    void peopleByFirestationAddress() throws Exception {
+        mockMvc.perform(get("/fire/200 Rue du Martre"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void peopleByStationNumber() throws Exception {
+        mockMvc.perform(get("/flood/2"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void peopleByName() throws Exception {
+        mockMvc.perform(get("/personInfo/Bruce+Wayne"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    void testGetEmailsInCity() throws Exception {
+        mockMvc.perform(get("/communityEmail/Culver"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 
 
 }
