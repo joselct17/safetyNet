1e314f5bb6c24df0110775b22d331fe4cccb2379
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-08 14:31:56 +0100

    rajout du test Post medicalRecords

--- src/test/java/com/safetynet/safetyNet/controller/MedicalRecordsControllerTest.java
+++ src/test/java/com/safetynet/safetyNet/controller/MedicalRecordsControllerTest.java
@@ -2,16 +2,27 @@ package com.safetynet.safetyNet.controller;
 
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.safetynet.safetyNet.dao.IMedicalRecordsDao;
+import com.safetynet.safetyNet.model.MedicalRecords;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
+import java.util.ArrayList;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -31,12 +42,35 @@ class MedicalRecordsControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Mock
+    IMedicalRecordsDao iMedicalRecordsDao;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    ObjectWriter objectWriter = objectMapper.writer();
+
 
     @Test
     void testPersonList() throws Exception {
         mockMvc.perform(get("/medicalRecord"))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+
+
+    @Test
+    void testPostMedicalRecords() throws Exception {
+
+        MedicalRecords medicalRecordsMock = new MedicalRecords("James", "McAvoy", "03/06/1984", new ArrayList<>(), new ArrayList<>());
+        Mockito.when(iMedicalRecordsDao.save(medicalRecordsMock)).thenReturn(medicalRecordsMock);
+        String content = objectWriter.writeValueAsString(medicalRecordsMock);
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/person")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content);
+        mockMvc.perform(mockRequest)
+                .andExpect(status().is(201));
     }
 
 
