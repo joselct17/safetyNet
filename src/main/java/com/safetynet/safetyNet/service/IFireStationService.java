0a39c7fe67dd79662c5e001f94acf74c4c87c7dc
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-02-09 18:01:54 +0100

    creation de la fonction getAllFirestation

--- src/main/java/com/safetynet/safetyNet/service/IFireStationService.java
+++ src/main/java/com/safetynet/safetyNet/service/IFireStationService.java
@@ -2,6 +2,8 @@ package com.safetynet.safetyNet.service;
 
 import com.safetynet.safetyNet.model.FireStation;
 
+import java.util.List;
+
 public interface IFireStationService {
 
     FireStation saveFirestation(FireStation fireStation);
@@ -12,5 +14,5 @@ public interface IFireStationService {
     void deleteFirestation(String address, String stationNumber);
 
 
-
+    List<FireStation> getAllFirestation();
 }
